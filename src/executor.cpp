ee7764b610ac99b00953a1b8211872555ece4138
Bipras Shrestha <biprasstha@gmail.com>
2026-06-26 18:24:46 -0700

    feat: add external process execution

--- src/executor.cpp
+++ src/executor.cpp
@@ -1,6 +1,32 @@
 #include "shell/executor.hpp"
 #include <cstdlib>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <iostream>
 
 void Executor::execute(const std::vector<std::string>& args) {
-    
+    if (args.empty()) {
+        return;
+    }
+
+    pid_t pid = fork();
+
+    if (pid < 0) { // fork failure
+        std::cerr << "Fork failed." << std::endl;
+    }
+    else if (pid == 0) { // child process
+        std::vector<char*> c_args;
+        for (const auto& arg : args) {
+            c_args.push_back(const_cast<char*>(arg.c_str()));
+        }
+        c_args.push_back(nullptr); // execvp requires a null-terminated array
+        execvp(c_args[0], c_args.data());
+        perror(c_args[0]);
+        _exit(EXIT_FAILURE);
+    }
+    else { // parent process
+        int status;
+        waitpid(pid, &status, 0);
+    }
 }
