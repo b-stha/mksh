56c0a2906f200e144e26dd720f9e7c1e87fe5e33
Bipras Shrestha <biprasstha@gmail.com>
2026-06-28 16:26:43 -0700

    add builtin logic

--- src/executor.cpp
+++ src/executor.cpp
@@ -1,4 +1,5 @@
 #include "shell/executor.hpp"
+#include "shell/builtin.hpp"
 #include <cstdlib>
 #include <unistd.h>
 #include <sys/types.h>
@@ -10,6 +11,14 @@ void Executor::execute(const std::vector<std::string>& args) {
         return;
     }
 
+
+    if (mksh::builtin::isBuiltin(args[0])) { // check if builtin
+        // execute builtin command
+        mksh::builtin::run(args);
+    }
+
+
+    // execute command externally
     pid_t pid = fork();
 
     if (pid < 0) { // fork failure
