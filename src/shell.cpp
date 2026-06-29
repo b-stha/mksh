ca5f1826017d140940a21ae8c495791f64be2142
Bipras Shrestha <biprasstha@gmail.com>
2026-06-29 11:17:53 -0700

    add cwd to shell prompt

--- src/shell.cpp
+++ src/shell.cpp
@@ -1,10 +1,16 @@
 #include "shell/shell.hpp"
 #include "shell/executor.hpp"
 #include <iostream>
+#include <limits.h>
+#include <unistd.h>
 
 void Shell::run() {
     while (true) {
-        std::cout << "mksh> ";
+        char cwdPath[PATH_MAX];
+        if (getcwd(cwdPath, sizeof(cwdPath)) == nullptr) {
+            perror("pwd");
+        }
+        std::cout << "mksh: " << cwdPath << "> ";
         std::getline(std::cin, input);
         if (input == "exit") {
             break;
